4071d3c2879f2609691c3491474ed59841e95e82
Ivan R. <xvantz@gmail.com>
2026-02-12 15:01:04 +0700

    feat: dGPU vfio_pci

--- modules/system/virtualization.nix
+++ modules/system/virtualization.nix
@@ -4,4 +4,13 @@
   virtualisation.docker.autoPrune.enable = true;
 
   users.users.xvantz.extraGroups = ["docker"];
+
+  boot.initrd.kernelModules = ["vfio_pci" "vfio" "vfio_iommu_type1"];
+
+  boot.extraModprobeConfig = ''
+    options vfio-pci ids=10de:25a0
+    options vfio-pci ids=10de:2291
+  '';
+
+  boot.blacklistedKernelModules = ["nvidia" "nouveau"];
 }
