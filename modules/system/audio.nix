607fe0bb6a1204f0d84b3da8c956b8d98f72cdd2
Ivan R. <xvantz@gmail.com>
2026-02-18 21:07:53 +0700

    fix: rework audio config

--- modules/system/audio.nix
+++ modules/system/audio.nix
@@ -14,11 +14,27 @@
         };
       };
       wireplumber.extraConfig."11-bluetooth-policy" = {
+        "wireplumber.settings" = {
+          "bluetooth.autoswitch-to-headset-profile" = false;
+        };
         "monitor.bluez.properties" = {
           "bluez5.enable-sbc-xq" = true;
           "bluez5.enable-msbc" = true;
-          "bluez5.codecs" = ["ldac" "aptx_hd" "aac" "sbc"];
+          "bluez5.roles" = ["hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" "a2dp_sink" "a2dp_source"];
         };
+        "monitor.alsa.rules" = [
+          {
+            matches = [
+              {"node.name" = "~alsa_input.*";}
+              {"node.name" = "~alsa_output.*";}
+            ];
+            actions = {
+              update-props = {
+                "session.suspend-timeout-seconds" = 0;
+              };
+            };
+          }
+        ];
       };
     };
   };
