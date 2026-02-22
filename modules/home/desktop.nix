f321eb83743ccefb8f1e909da3aa458273e276b6
Ivan R. <xvantz@gmail.com>
2026-02-22 18:31:54 +0700

    feat: delete hyperidle and hyperlock

--- modules/home/desktop.nix
+++ modules/home/desktop.nix
@@ -1,43 +1,8 @@
-{
-  pkgs,
-  config,
-  selfPath,
-  ...
-}: {
+{pkgs, ...}: {
   programs.niri = {
     enable = true;
     package = pkgs.niri;
     settings = {};
-  };
-
-  programs.hyprlock.enable = true;
-
-  xdg.configFile."hypr/hyprlock.conf".source = config.lib.file.mkOutOfStoreSymlink "${selfPath}/config/hypr/hyprlock.conf";
-  xdg.configFile."hypr/mocha.conf".source = config.lib.file.mkOutOfStoreSymlink "${selfPath}/config/hypr/mocha.conf";
-  xdg.configFile."hypr/nix-black.png".source = config.lib.file.mkOutOfStoreSymlink "${selfPath}/config/hypr/nix-black.png";
-  xdg.configFile."hypr/logo.png".source = config.lib.file.mkOutOfStoreSymlink "${selfPath}/config/hypr/logo.png";
-
-  services.hypridle = {
-    enable = true;
-    settings = {
-      general = {
-        lock_cmd = "pidof hyprlock || hyprlock";
-        before_sleep_cmd = "loginctl lock-session && ${pkgs.playerctl}/bin/playerctl pause";
-        after_sleep_cmd = "niri msg action power-on-monitors";
-      };
-
-      listener = [
-        {
-          timeout = 600;
-          on-timeout = "loginctl lock-session";
-        }
-        {
-          timeout = 1000;
-          on-timeout = "niri msg action power-off-monitors";
-          on-resume = "niri msg action power-on-monitors";
-        }
-      ];
-    };
   };
 
   xdg.portal = {
