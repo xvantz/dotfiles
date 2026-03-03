1822fdfb400877144f6325181c7dbc2753111807
Ivan R. <xvantz@gmail.com>
2026-03-03 17:48:05 +0700

    feat: add hyperidle

--- modules/home/desktop.nix
+++ modules/home/desktop.nix
@@ -5,6 +5,29 @@
     settings = {};
   };
 
+  services.hypridle = {
+    enable = true;
+    settings = {
+      general = {
+        lock_cmd = "loginctl lock-session";
+        before_sleep_cmd = "loginctl lock-session && playerctl pause";
+        after_sleep_cmd = "niri msg action power-on-monitors";
+      };
+
+      listener = [
+        {
+          timeout = 600;
+          on-timeout = "loginctl lock-session";
+        }
+        {
+          timeout = 1000;
+          on-timeout = "niri msg action power-off-monitors";
+          on-resume = "niri msg action power-on-monitors";
+        }
+      ];
+    };
+  };
+
   xdg.portal = {
     enable = true;
     xdgOpenUsePortal = true;
