a868414d7f9b5a26aa478b4c03f6f442267d821b
Ivan R. <xvantz@gmail.com>
2026-03-03 17:47:52 +0700

    feat: add zen logic for clear tabs

--- modules/home/browser.nix
+++ modules/home/browser.nix
@@ -6,6 +6,7 @@
   programs.zen-browser = {
     enable = true;
     policies = {
+      DisableTelemetry = true;
       ExtensionSettings = {
         "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/file/4664623/bitwarden_password_manager-2025.12.1.xpi";
@@ -22,6 +23,40 @@
         "sponsorBlocker@ajay.app" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/sponsorblock/latest.xpi";
           installation_mode = "force_installed";
+        };
+      };
+      Preferences = {
+        "browser.tabs.unloadOnLowMemory" = {
+          Value = true;
+          Status = "locked";
+        };
+        "browser.low_mem_strategy_aggressive" = {
+          Value = true;
+          Status = "locked";
+        };
+        "dom.ipc.processCount" = {
+          Value = 4;
+          Status = "locked";
+        };
+        "browser.sessionstore.restore_on_demand" = {
+          Value = true;
+          Status = "locked";
+        };
+        "browser.sessionstore.restore_tabs_lazily" = {
+          Value = true;
+          Status = "locked";
+        };
+        "browser.tabs.min_inactive_duration_before_unload" = {
+          Value = 1200000;
+          Status = "locked";
+        };
+        "browser.tabs.exceed_soft_memory_limit" = {
+          Value = true;
+          Status = "locked";
+        };
+        "browser.tabs.soft_memory_limit_mb" = {
+          Value = 6144;
+          Status = "locked";
         };
       };
     };
