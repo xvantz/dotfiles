8f4593b155c70cf9b57c63906c6d7a65f4e26a7d
Ivan R. <xvantz@gmail.com>
2026-04-04 15:58:39 +0700

    feat: add time sync services

--- modules/system/services.nix
+++ modules/system/services.nix
@@ -4,6 +4,11 @@
   services.dbus.implementation = "broker";
   security.pam.services.hyprlock = {};
   services.fstrim.enable = true;
+  services.timesyncd.enable = false;
+  services.chrony = {
+    enable = true;
+    servers = ["pool.ntp.org"];
+  };
 
   systemd.user.extraConfig = "DefaultTimeoutStopSec=10s";
 }
