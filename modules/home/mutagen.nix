06783796b446392bf77b912d582f4969f023d7f9
Ivan R. <xvantz@gmail.com>
2026-05-13 16:22:55 +0700

    feat: add mutagen sync for virtual windows

--- modules/home/mutagen.nix
+++ modules/home/mutagen.nix
@@ -1,4 +1,8 @@
 {pkgs, ...}: {
+  home.packages = with pkgs; [
+    mutagen
+  ];
+
   systemd.user.services.mutagen-daemon = {
     Unit.Description = "Mutagen Terminal Daemon";
     Service = {
@@ -18,17 +22,22 @@
 
     Service = {
       Type = "simple";
-      Path = with pkgs; [openssh mutagen coreutils];
       ExecStart = pkgs.writeShellScript "start-cocos-sync" ''
         sleep 2
-        mutagen sync terminate cocos-dev >/dev/null 2>&1 || true
-        mutagen sync create \
+
+        ${pkgs.mutagen}/bin/mutagen sync terminate cocos-dev >/dev/null 2>&1 || true
+
+        ${pkgs.mutagen}/bin/mutagen sync create \
           --name=cocos-dev \
-          --ignore="node_modules,.git,library,local,temp,build" \
+          -m two-way-resolved \
+          --ignore="node_modules,.git,library,local,temp" \
+          --stage-mode-beta=neighboring \
+          --watch-mode-beta=force-poll \
+          --watch-polling-interval-beta=1 \
           /home/xvantz/projects/cocos \
           xvantz@192.168.122.157:C:/Users/xvantz/projects/cocos-projects
 
-        exec tail -f /dev/null
+        exec ${pkgs.coreutils}/bin/tail -f /dev/null
       '';
       ExecStop = "${pkgs.mutagen}/bin/mutagen sync terminate cocos-dev";
       Restart = "always";
