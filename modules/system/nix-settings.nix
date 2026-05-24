e8cfa3dc02049c43b85f678f46234beceb11a811
Ivan R. <xvantz@gmail.com>
2026-05-24 20:04:56 +0700

    feat: change nix to lix

--- modules/system/nix-settings.nix
+++ modules/system/nix-settings.nix
@@ -1,4 +1,17 @@
-{...}: {
+{pkgs, ...}: {
+  nixpkgs.overlays = [
+    (final: prev: {
+      inherit
+        (prev.lixPackageSets.stable)
+        nix-eval-jobs
+        nix-fast-build
+        nix-direnv
+        ;
+    })
+  ];
+
+  nix.package = pkgs.lixPackageSets.stable.lix;
+
   nixpkgs.config = {
     allowUnfree = true;
     permittedInsecurePackages = [
