71a420f52e35987d74a23e0174cff2ba5b4de7f8
Ivan R. <xvantz@gmail.com>
2026-05-30 22:34:20 +0700

    feat: update tiny plugin neovim config

--- modules/home/neovim/plugins/tiny-inline-diagnostic.nix
+++ modules/home/neovim/plugins/tiny-inline-diagnostic.nix
@@ -1,3 +1,43 @@
 {...}: {
-  programs.nixvim.plugins.tiny-inline-diagnostic.enable = true;
+  programs.nixvim.plugins.tiny-inline-diagnostic = {
+    enable = true;
+
+    settings = {
+      preset = "modern";
+      transparent_bg = false;
+      transparent_cursorline = true;
+
+      options = {
+        show_source = {
+          enabled = true;
+          if_many = true;
+        };
+
+        show_code = true;
+
+        use_icons_from_diagnostic = true;
+        set_arrow_to_diag_color = true;
+        throttle = 20;
+
+        show_all_diags_on_cursorline = true;
+
+        show_related = {
+          enabled = true;
+          max_count = 3;
+        };
+
+        enable_on_insert = false;
+
+        multilines = {
+          enabled = true;
+          always_show = false;
+          trim_whitespaces = true;
+        };
+
+        overflow = {
+          mode = "wrap";
+        };
+      };
+    };
+  };
 }
