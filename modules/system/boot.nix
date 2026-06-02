0aa7d4016a11bc90cb13b3543088865dfb6cefc1
Ivan R. <xvantz@gmail.com>
2026-06-03 00:02:54 +0700

    change swapiness

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -28,7 +28,7 @@
     ];
 
     kernel.sysctl = {
-      "vm.swappiness" = 180;
+      "vm.swappiness" = 10;
       "vm.watermark_boost_factor" = 0;
       "vm.watermark_scale_factor" = 125;
       "vm.page-cluster" = 0;
