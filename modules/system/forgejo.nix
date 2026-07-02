a95a5f655757d1f10e50b0cd2f005dabf4d6bfb3
Ivan R. <xvantz@gmail.com>
2026-07-02 22:59:09 +0700

    chore: refactor forgejo

--- modules/system/forgejo.nix
+++ modules/system/forgejo.nix
@@ -5,17 +5,18 @@
 }: let
   srv = config.services.forgejo.settings.server;
 in {
-  sops.secrets.forgejo_runner_token = {};
-  sops.secrets.forgejo_ssh_key = {
-    path = "/var/lib/forgejo/.ssh/id_ed25519";
-    owner = "forgejo";
-  };
-  sops.secrets.github_admin_env = {
-    owner = "xvantz";
-  };
-
-  sops.secrets.forgejo_admin_env = {
-    owner = "xvantz";
+  sops.secrets = {
+    forgejo_runner_token = {};
+    forgejo_ssh_key = {
+      path = "/var/lib/forgejo/.ssh/id_ed25519";
+      owner = "forgejo";
+    };
+    github_admin_env = {
+      owner = "xvantz";
+    };
+    forgejo_admin_env = {
+      owner = "xvantz";
+    };
   };
 
   services.forgejo = {
@@ -46,46 +47,48 @@ in {
     ${pkgs.openssh}/bin/ssh-keyscan github.com 2>/dev/null >> /var/lib/forgejo/.ssh/known_hosts || true
   '';
 
-  services.gitea-actions-runner = {
-    package = pkgs.forgejo-runner;
-    instances.default = {
-      enable = true;
-      name = "nixos-runner";
-      url = "http://127.0.0.1:2000/";
-      tokenFile = config.sops.secrets.forgejo_runner_token.path;
-      labels = ["ubuntu-latest:docker://node:20-bookworm"];
-      settings = {
-        container = {
-          network = "host";
-          privileged = false;
-          options = "--memory=4g --cpus=2";
-          docker_host = "automount";
+  services = {
+    gitea-actions-runner = {
+      package = pkgs.forgejo-runner;
+      instances.default = {
+        enable = true;
+        name = "nixos-runner";
+        url = "http://127.0.0.1:2000/";
+        tokenFile = config.sops.secrets.forgejo_runner_token.path;
+        labels = ["ubuntu-latest:docker://node:20-bookworm"];
+        settings = {
+          container = {
+            network = "host";
+            privileged = false;
+            options = "--memory=4g --cpus=2";
+            docker_host = "automount";
+          };
+          log.level = "info";
         };
-        log.level = "info";
       };
     };
-  };
 
-  services.forgejo-sync = {
-    enable = true;
-
-    forgejo = {
-      url = "http://localhost:2000";
-      tokenFile = config.sops.secrets.forgejo_admin_env.path;
-    };
-
-    platforms.github = {
+    forgejo-sync = {
       enable = true;
-      tokenFile = config.sops.secrets.github_admin_env.path;
-    };
 
-    import = {
-      enable = true;
-      schedule = "hourly";
-    };
+      forgejo = {
+        url = "http://localhost:2000";
+        tokenFile = config.sops.secrets.forgejo_admin_env.path;
+      };
 
-    pushMirrors.enable = true;
-    autoCreate.enable = true;
+      platforms.github = {
+        enable = true;
+        tokenFile = config.sops.secrets.github_admin_env.path;
+      };
+
+      import = {
+        enable = true;
+        schedule = "hourly";
+      };
+
+      pushMirrors.enable = true;
+      autoCreate.enable = true;
+    };
   };
 
   environment.systemPackages = [config.services.forgejo-sync.package];
