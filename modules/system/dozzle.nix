d730fd10f517699c47455ac772e7bd425c41b9f3
Ivan R. <xvantz@gmail.com>
2026-07-24 01:28:57 +0700

    fix: dozzle socket

--- modules/system/dozzle.nix
+++ modules/system/dozzle.nix
@@ -1,8 +1,8 @@
-{ config, pkgs, ... }: {
+{...}: {
   virtualisation.oci-containers.containers.dozzle = {
     image = "docker.io/amir20/dozzle:latest";
-    ports = [ "127.0.0.1:9999:8080" ];
-    volumes = [ "/var/run/docker.sock:/var/run/docker.sock:ro" ];
-    extraOptions = [ "--pull=always" ];
+    ports = ["127.0.0.1:9999:8080"];
+    volumes = ["/run/podman/podman.sock:/var/run/docker.sock:ro"];
+    extraOptions = ["--pull=always"];
   };
 }
