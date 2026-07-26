4b767040fcbbf9a78eba5e093caf72b209518633
Ivan R. <xvantz@gmail.com>
2026-07-26 20:05:09 +0700

    feat: configuration keepassxc

--- modules/home/keepassxc.nix
+++ modules/home/keepassxc.nix
@@ -3,17 +3,42 @@
     enable = true;
     autostart = true;
     settings = {
+      GUI = {
+        ShowTrayIcon = true;
+        MinimizeOnClose = true;
+        MinimizeToTray = true;
+      };
+
       Browser = {
         Enabled = true;
       };
 
       SSHAgent = {
         Enabled = true;
+        UseOpenSSH = true;
       };
 
       FdoSecrets = {
         Enabled = true;
       };
+
+      Security = {
+        LockDatabaseScreenLock = true;
+        LockDatabaseIdleSeconds = 600;
+        ClearClipboardTimeout = 10;
+      };
+
+
+      PasswordGenerator = {
+        Length = 25;
+      };
+
+      General = {
+        AutoReloadOnChange = true;
+        BackupBeforeSave = true;
+      };
     };
   };
+
+  xdg.autostart.enable = true;
 }
