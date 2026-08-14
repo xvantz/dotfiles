071638474f235f8f2d392b3126567a20b3bd8fe5
Ivan R. <xvantz@gmail.com>
2026-08-15 01:49:56 +0700

    feat: update hermes agent

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
 
     sync-agent.url = "git+http://localhost:2000/xvantz/sync-agent.git";
 
-    hermes-agent.url = "github:NousResearch/hermes-agent/v2026.7.30";
+    hermes-agent.url = "github:NousResearch/hermes-agent/v2026.8.3";
 
     nixvim = {
       url = "github:nix-community/nixvim";
