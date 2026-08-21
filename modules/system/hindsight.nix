5cf65423f4199c398ca54655eaff9b190cedb313
Ivan R. <xvantz@gmail.com>
2026-08-22 00:55:47 +0700

    feat: add hindsight

--- modules/system/hindsight.nix
+++ modules/system/hindsight.nix
@@ -2,30 +2,21 @@
   pkgs,
   config,
   ...
-}: let
-  hindsightDir = "/var/lib/hindsight";
-in {
+}: {
+  sops.secrets.hindsight_env = {
+    owner = "xvantz";
+  };
+
   virtualisation.oci-containers.containers.hindsight = {
     image = "ghcr.io/vectorize-io/hindsight:latest";
-    ports = ["127.0.0.1:8891:8888" "127.0.0.1:9999:9999"];
-    volumes = ["${hindsightDir}/data:/home/hindsight/.pg0:Z"];
+    ports = ["127.0.0.1:8891:8888" "127.0.0.1:9998:9999"];
+    volumes = ["hindsight-data:/home/hindsight/.pg0"];
     extraOptions = ["--pull=always"];
     environment = {
       HINDSIGHT_API_LLM_PROVIDER = "openai";
       HINDSIGHT_API_LLM_BASE_URL = "https://opencode.ai/zen/go/v1";
       HINDSIGHT_API_LLM_MODEL = "mimo-v2.5";
     };
-    # Use environmentFiles for secrets
     environmentFiles = [config.sops.secrets.hindsight_env.path];
-  };
-
-  systemd.services.hindsight-data = {
-    description = "Ensure Hindsight data directory exists";
-    before = ["container-hindsight.service"];
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = true;
-      ExecStart = "${pkgs.coreutils}/bin/mkdir -p ${hindsightDir}/data";
-    };
   };
 }
